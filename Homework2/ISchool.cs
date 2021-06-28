0375a27aa1bf3454143298d933f5f3a1a9f606b1
Tornike Giunashvili <t.giunashvili@area.ge>
2021-06-28 11:24:36 +0400

    Add project files.

---
+++ Homework2/ISchool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    public interface ISchool
+    {
+        void AddPerson(string name, int point);
+
+        void SetPoint(string name, int point);
+
+        void RemovePerson(string name);
+
+        void GetPersonPoint(string name);
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Person = new School();
+            Person.AddPerson("tornike");
+            Person.SetPoint("tornike", 7);
+            Person.GetPersonPoint("tornike");
+        }
+    }
+}
